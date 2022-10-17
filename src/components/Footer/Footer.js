bb2c5b4cda2354cccce1111b6237485e848d6224
ilham bouaik <ibouaik@gmail.com>
2022-10-17 18:20:01 +0100

    add footer style

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import "./footer.scss"
 
 function Footer() {
     return (
-        <div>Footer</div>
+        <div className="footer">
+            <div>Movie App</div>
+            <div>@2022 by Ilham Bouaik</div>
+        </div>
     )
 }
 
